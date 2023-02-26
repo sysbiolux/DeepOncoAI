840580ef192374f68952ed0cd67121f7c483bd3a
Seb <sebdelandtsheer@gmail.com>
2023-02-26 18:03:50 +0100

    FINAL 2

--- celltype-analysis.py
+++ celltype-analysis.py
@@ -4,8 +4,16 @@
 import numpy as np
 import pandas as pd
 from config import Config
-
+from matplotlib import pyplot as plt
 from DBM_toolbox.data_manipulation.data_utils import pickle_objects, unpickle_objects
+from sklearn.metrics import roc_curve, auc
+import seaborn as sns
+import colorlover as cl
+
+
+
+###############################################################################
+### 1 Tables with performances per cell type
 
 final_results = unpickle_objects('FINAL_results_2023-02-20-12-35-21-577662.pkl')
 config = Config("testall/config_paper.yaml")
@@ -92,5 +100,72 @@ df_prec.to_csv('FINAL_celltype_results_prec.csv')
 df_recall.to_csv('FINAL_celltype_results_recall.csv')
 
 #####################################################################
-#####################################################################
+##################| ROC Curves
 
+targets_names = list(final_results.keys())
+fprs = dict()
+tprs = dict()
+roc_aucs = dict()
+
+for target_name in targets_names:
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    global_models = [x for x in final_results[target_name].columns if x.startswith('pred2')]
+    plt.figure()
+    for global_model in global_models:
+        y_score = final_results[target_name].loc[:, global_model]
+        y_score = y_score[y_score.isna() == False]
+        y_true = final_results[target_name].loc[:, target_name]
+        y_true = y_true.loc[y_score.index]
+        fpr[global_model], tpr[global_model], _ = roc_curve(y_true, y_score)
+        roc_auc[global_model] = auc(fpr[global_model], tpr[global_model])
+        plt.plot(fpr[global_model], tpr[global_model], label=f"AUC: {round(roc_auc[global_model], 3)}")
+    plt.plot([0, 1], [0, 1], color="black", linestyle="--")
+    plt.xlim([0.0, 1.05])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title(f"{target_name.split('_')[0]}")
+    plt.legend(loc="lower right")
+    plt.savefig(f'FINAL_ROC_{target_name}')
+
+    fprs[target_name] = fpr
+    tprs[target_name] = tpr
+    roc_aucs[target_name] = roc_auc
+
+
+
+
+
+
+
+
+###################################################################
+
+fi = unpickle_objects('FINAL_featimp_2023-02-20-12-35-21-592650.pkl')
+base_models = unpickle_objects('FINAL_base-models_2023-02-20-12-35-21-611629.pkl')
+
+fi_names = fi['Lapatinib_ActArea'][0]['RFC'].index
+
+res_fi = pd.DataFrame(0, columns=fi_names, index=fi.keys())
+
+for target_name, target_dict in fi.items():
+    these_means = pd.Series()
+    for loop_number, loop_dict in target_dict.items():
+        these_means = pd.concat([these_means, loop_dict['RFC']], axis=1)
+    this_mean = these_means.mean(axis=1)
+    res_fi.loc[target_name, :] = this_mean
+
+colnames = res_fi.columns
+colnames = ['+'.join(x.split('_')[1:]) for x in colnames]
+
+res_fi.columns = colnames
+
+palette = cl.scales['10']['div']['Spectral']
+
+# Create the heatmap using seaborn
+sns.heatmap(res_fi, cmap=palette, annot=True, fmt='.2f')
+
+# Display the heatmap
+plt.show()
